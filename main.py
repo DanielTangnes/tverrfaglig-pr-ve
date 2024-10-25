a20c1b7a024a8a6dc491bc52ed220fd744d85d97
fuzeOW <avidsexualoffender@gmail.com>
2024-10-25 09:58:41 +0200

    qwerty

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-test
+import tkinter as tk
+import pyodbc
+
+server = 'your_server_name'
+database = 'your_database_name'
+username = 'your_username'
+password = 'your_password'
+
+# Establishing a connection to the SQL Server
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};\
+                      SERVER='+server+';\
+                      DATABASE='+database+';\
+                      UID='+username+';\
+                      PWD='+ password)
+
+cursor = cnxn.cursor()
+
+def connect_db():
+
+
+def display_varelager():
