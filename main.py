8360c9c3f3433d80c28928a0433db5a4192a2e77
fuzeOW <avidsexualoffender@gmail.com>
2024-10-25 11:24:06 +0200

    Lagt til display av tabell i GUI

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import tkinter as tk
+from tkinter import ttk
 import mysql.connector
 
-server = 'daniels-MacBook-Pro.local'
-database = 'varehusdb'
-username = 'root'
-password = ''
+def connect_DB():
+    server = 'daniels-MacBook-Pro.local'
+    database = 'varehusdb'
+    username = 'root'
+    password = ''
 
-# Establishing a connection to the MySQL database
-cnxn = mysql.connector.connect(
-    host=server,
-    database=database,
-    user=username,
-    password=password
-)
+    # Establishing a connection to the MySQL database
+    cnxn = mysql.connector.connect(
+        host=server,
+        database=database,
+        user=username,
+        password=password
+    )
 
-cursor = cnxn.cursor()
+    cursor = cnxn.cursor()
 
-# Example query
-cursor.execute("SHOW TABLES")
-for table in cursor:
-    print(table)
+    cursor.execute("select * from ansatt")
+    rows = cursor.fetchall()
+    cnxn.close()
 
-# Remember to close the cursor and connection when done
-cursor.close()
-cnxn.close()
+    return rows
+
+### Displayer tabell i GUI
+root = tk.Tk()
+root.title("test display")
+root.geometry("600x400")
+
+tree = ttk.Treeview(root)
+tree['columns'] = ("test1", "test2", "test3")
+
+rows = connect_DB()
+for row in rows:
+    tree.insert("", "end", values=row)
+
+tree.pack(pady=20)
+
+root.mainloop()
