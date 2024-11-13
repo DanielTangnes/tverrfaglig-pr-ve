7b7fac374e95082380120cb5088fe4947a31e85b
od-je <od-jen@live.no>
2024-11-13 19:21:30 +0100

    endring

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def connect_DB():
     server = 'localhost' # Husk å endre til riktig servernavn (kan kjøre SHOW VARIABLES LIKE '%hostname%' for å finne navn)
     database = 'varehusdb'
     username = 'root'
-    password = 'passord123'
+    password = 'Passord'
 
     connection = mysql.connector.connect(
         host=server,
@@ -24,7 +24,7 @@ def ordre():
     rows = cur1.fetchall()
     for row in rows:
         print(row)
-        tree2.insert('', 'end', values=row)
+        tree1.insert('', 'end', values=row)
     con1.close()
 
 def kunde():
@@ -34,8 +34,9 @@ def kunde():
     rows = cur2.fetchall()
     for row in rows:
         print(row)
-        tree1.insert('', 'end', values=row)
+        tree2.insert('', 'end', values=row)
     con2.close()
+
 
 ### Displayer tabell i GUI
 root = tk.Tk()
@@ -52,18 +53,6 @@ tabControl.add(tab2, text="Ordre")
 
 tabControl.pack(expand=1, fill="both")
 
-
-tree1 = ttk.Treeview(tab1, column=("c1", "c2", "c3"), show='headings')
-tree1.column("#1", anchor=tk.CENTER)
-tree1.heading("#1", text="ID")
-tree1.column("#2", anchor=tk.CENTER)
-tree1.heading("#2", text="FNAME")
-tree1.column("#3", anchor=tk.CENTER)
-tree1.heading("#3", text="LNAME")
-tree1.pack()
-button1 = tk.Button(tab1, text="Oppdater ordre", command= kunde)
-button1.pack(pady=10)
-
 tree2 = ttk.Treeview(tab2, column=("c1", "c2", "c3", "c4", "c5"), show='headings')
 tree2.column("#1", anchor=tk.CENTER)
 tree2.heading("#1", text="OrdreNr")
@@ -76,7 +65,21 @@ tree2.heading("#4", text="BetaltDato")
 tree2.column("#5", anchor=tk.CENTER)
 tree2.heading("#5", text="KundeNr")
 tree2.pack()
-button2 = tk.Button(tab2, text="Oppdater kunde", command= ordre)
+
+
+tree1 = ttk.Treeview(tab1, column=("c1", "c2", "c3"), show='headings')
+tree1.column("#1", anchor=tk.CENTER)
+tree1.heading("#1", text="ID")
+tree1.column("#2", anchor=tk.CENTER)
+tree1.heading("#2", text="FNAME")
+tree1.column("#3", anchor=tk.CENTER)
+tree1.heading("#3", text="LNAME")
+tree1.pack()
+button1 = tk.Button(tab2, text="Oppdater ordre", command= kunde)
+button1.pack(pady=10)
+button2 = tk.Button(tab1, text="Oppdater kunde", command= ordre)
 button2.pack(pady=10)
+
+
 
 root.mainloop()
