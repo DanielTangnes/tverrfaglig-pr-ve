b28c42a683760fa2a028680e5add69906fcfcec5
od-je <od-jen@live.no>
2024-11-28 19:28:11 +0100

    postnr_entry til postnr_combo

--- main.py
+++ main.py
@@ -160,11 +160,11 @@ def postkoder():
             connection.close()
 
 if __name__ == '__main__':
-    root, tree1, tree2, tree3, fornavn_entry, etternavn_entry, adresse_entry, postnr_entry, selected_knr = gui(
+    root, tree1, tree2, tree3, fornavn_entry, etternavn_entry, adresse_entry, postnr_combo, selected_knr = gui(
         hent_ordreliste_cmd=lambda: ordre_db(tree2),
         hent_kundeliste_cmd=lambda: kunde_db(tree1),
         varehus_cmd=lambda: varehus_db(tree3),
-        opprett_kunde_cmd=lambda: opprette_kunde(fornavn_entry.get(), etternavn_entry.get(), adresse_entry.get(), postnr_entry.get()),
+        opprett_kunde_cmd=lambda: opprette_kunde(fornavn_entry.get(), etternavn_entry.get(), adresse_entry.get(), postnr_combo.get()),
         slett_kunde_cmd=lambda: slett_kunde(selected_knr.get()),
         hent_valgt_ordre_cmd=lambda ordrenr, tree: valgt_ordre_db(ordrenr, tree),
         postkoder_cmd=lambda: postkoder()
